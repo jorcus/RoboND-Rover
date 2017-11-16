96276c35a4f5c4f8232644f2000aa925c6dd4967
NG FANG KIANG <kiang.ng@hotmail.com>
2017-11-16 10:34:30 +0800

    updates report

--- code/decision.py
+++ code/decision.py
@@ -5,7 +5,6 @@ import random
 # This is where you can build a decision tree for determining throttle, brake and steer 
 # commands based on the output of the perception_step() function
 def decision_step(Rover):
-
     # Implement conditionals to decide what to do given perception data
     # Here you're all set up with some basic functionality but you'll need to
     # improve on this decision tree to do a good job of navigating autonomously!
